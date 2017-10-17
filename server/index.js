86782fc845a1027b46dc352da719a40aca224171
DanielKell <danielkell.ubc@gmail.com>
2017-10-16 22:36:53 -0700

    First Commit
    Setup basic server with Express. Included some middleware

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express'); //This is ES5 format
+
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+//App Setup
+app.use(morgan('combined')); //A logging framework. Logs a request for debugging
+app.use(bodyParser.json({type: '*/*'})); //Parses incoming requests, into JSON
+//Both of these are middleware. Incoming request is passed through these first.
+//Nodemon watches our project directory for changes. If there are changes, it
+//updates the server automatically
+
+
+
+//Server Setup
+
+
+
+
+//Define a port. If there is a port defined use that, otherwise use 3090
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+//Create a HTTP server that knows how to receive requests, and then forward to express.
+
+server.listen(port);
+console.log('Server listening on:', port);
